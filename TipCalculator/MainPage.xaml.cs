1b314008f8911e9c17a1e7767297f00daf5394c4
= <=>
2024-08-23 22:43:06 -0300

    Adicionei a lógica do calculo da gorjeta

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -11,12 +11,14 @@
 
         private void SetLowTip_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = 15;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void SetHighTip_Clicked(System.Object sender, System.EventArgs e)
         {
-
+            TipSlider.Value = 20;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void RoundUpButton_Clicked(System.Object sender, System.EventArgs e)
@@ -27,6 +29,25 @@
         private void RoundDownButton_Clicked(System.Object sender, System.EventArgs e)
         {
 
+        }
+
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
+        }
+
+        private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Pegar o valor inserido ex: 100
+            //Calcular o valor da gorjeta de acordo com a porcentagem
+            //Calcular o valor total, sendo a soma do valor + a gorjeta
+
+            double Valor = Convert.ToDouble(AmountEntry.Text);
+            double ValorDaGorjeta = Valor * (TipSlider.Value/100);
+            double ValorTotal = ValorDaGorjeta + Valor;
+
+            TotalLabel.Text = ValorTotal.ToString("C");
+            TipLabel.Text = ValorDaGorjeta.ToString("C");
         }
     }
 
